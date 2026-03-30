90e315fc8245de686f45fca61b98b7241e9a9f76
hossamgasser <hossgasser@gmail.com>
2026-03-30 19:39:12 -0400

    fixed double user creation error

--- app/src/main/java/com/example/popin/MainActivity.java
+++ app/src/main/java/com/example/popin/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.popin;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 import androidx.activity.EdgeToEdge;
@@ -41,6 +42,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onSuccess(User user) {
                 Toast.makeText(getApplicationContext(), "Welcome " + user.getName(), Toast.LENGTH_SHORT).show();
+
                 UserInSession.create(user);
 
                 Intent intent = new Intent(MainActivity.this, EventsPageActivity.class);
--- app/src/main/java/com/example/popin/User.java
+++ app/src/main/java/com/example/popin/User.java
@@ -118,7 +118,6 @@ public class User {
                             callback.onSuccess(User.this);
                         }
 
-                        callback.onSuccess(User.this);
                         return;
                     } else {
                         callback.onError("Incorrect password");
